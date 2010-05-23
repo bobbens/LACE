fad65308e54ee27555f09dbe248f6bd6284ef286
Edgar Simo <bobbens@gmail.com>
2010-05-23 19:05:34 +0200

    * Doxyegn and modes.

--- modules/dhb/hbridge.h
+++ modules/dhb/hbridge.h
@@ -8,12 +8,19 @@
 /*
  * The commands.
  */
-#define HB_CMD_NONE     0x00
-#define HB_CMD_VERSION  0x01
-#define HB_CMD_MODESET  0x02
-#define HF_CMD_MODEGET  0x03
-#define HB_CMD_MOTORSET 0x04
-#define HB_CMD_MOTORGET 0x05
+#define DHB_CMD_NONE     0x00 /**< Invalid command. */
+#define DHB_CMD_VERSION  0x01 /**< Gets version. */
+#define DHB_CMD_MODESET  0x02 /**< Sets operating mode. */
+#define DHF_CMD_MODEGET  0x03 /**< Gets operating mode. */
+#define DHB_CMD_MOTORSET 0x04 /**< Sets motor velocity. */
+#define DHB_CMD_MOTORGET 0x05 /**< Gets motor velocity. */
+
+
+/*
+ * The modes.
+ */
+#define DHB_MODE_PWM    0x00 /**< PWM open loop mode. */
+#define DHB_MODE_FBKS   0x01 /**< Feedback closed loop mode. */
 
 
 #endif /* _HBRIDGE_H */
